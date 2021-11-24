61abd09fba87e6f169a7a645ba2fb2a13eddeb83
no-self-required <louism.coding@gmail.com>
2021-11-24 18:39:35 -0500

    set up input field

---
+++ tracker/src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react"
+
+function Main () {
+    
+    
+    return (
+        <div>x
+            <input type="text" id="timer" name="timer" maxLength="6"></input>
+        </div>
+    )
+}
+
+export default Main
