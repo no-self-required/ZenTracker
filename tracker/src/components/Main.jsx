0ea3296af126f4c066ae935ec0d587beff7907c4
no-self-required <louism.coding@gmail.com>
2021-11-27 15:48:59 -0500

    add to work on comments

--- tracker/src/components/Main.jsx
+++ tracker/src/components/Main.jsx
@@ -3,71 +3,68 @@ import React, { useState, useEffect } from "react";
 //add rendering logic to only show the right amount of time. ex: if there are 0hours, dont display
 // add logic for finished timer (0seconds)
 function splitInput(initialTime) {
-    const parsedTimer = parseInt(initialTime);
-    const arr = Array.from(parsedTimer.toString()).map(Number);
-    if (arr.length <= 5) {
-      const padAmount = 6 - arr.length;
-      const arr2 = [...Array(padAmount).fill(0), ...arr];
-      return arr2;
-    }
-    return arr;
+  const parsedTimer = parseInt(initialTime);
+  const arr = Array.from(parsedTimer.toString()).map(Number);
+  if (arr.length <= 5) {
+    const padAmount = 6 - arr.length;
+    const arr2 = [...Array(padAmount).fill(0), ...arr];
+    return arr2;
   }
+  return arr;
+}
 
-  function calculateSeconds(initialTime) {
-    const splitArr = splitInput(initialTime);
-    const seconds = [];
-    const minutes = [];
-    const hours = [];
-    hours.push(splitArr[0], splitArr[1]);
-    minutes.push(splitArr[2], splitArr[3]);
-    seconds.push(splitArr[4], splitArr[5]);
-    const totSec = seconds[0] * 10 + seconds[1];
-    const totMin = minutes[0] * 600 + minutes[1] * 60;
-    const totHours = hours[0] * 36000 + hours[1] * 3600;
-    const calculatedTotalSeconds = totSec + totMin + totHours;
-    return calculatedTotalSeconds 
-  }
+function calculateSeconds(initialTime) {
+  const splitArr = splitInput(initialTime);
+  const seconds = [];
+  const minutes = [];
+  const hours = [];
+  hours.push(splitArr[0], splitArr[1]);
+  minutes.push(splitArr[2], splitArr[3]);
+  seconds.push(splitArr[4], splitArr[5]);
+  const totSec = seconds[0] * 10 + seconds[1];
+  const totMin = minutes[0] * 600 + minutes[1] * 60;
+  const totHours = hours[0] * 36000 + hours[1] * 3600;
+  const calculatedTotalSeconds = totSec + totMin + totHours;
+  return calculatedTotalSeconds;
+}
 
 function Main() {
   const TIMER_STATES = {
-    INITIAL : 0,
-    STARTED : 1,
-    STOPPED : 2,
-    FINISHED : 3
-  }
+    INITIAL: 0,
+    STARTED: 1,
+    STOPPED: 2,
+    FINISHED: 3,
+  };
 
   const [totalSeconds, setTotalSeconds] = useState();
   const [initialTime, setInitialTime] = useState(500);
-  const [intervalID, setIntervalID] = useState()
-  const [timerState, setTimerState] = useState(TIMER_STATES["INITIAL"])
+  const [intervalID, setIntervalID] = useState();
+  const [timerState, setTimerState] = useState(TIMER_STATES["INITIAL"]);
 
   useEffect(() => {
     if (timerState === TIMER_STATES["STARTED"] && !intervalID) {
       console.log("Internal started.");
-      const intervalID =  setInterval(decrementTotalSeconds, 1000);
-      setIntervalID(intervalID)
+      const intervalID = setInterval(decrementTotalSeconds, 1000);
+      setIntervalID(intervalID);
     }
   }, [timerState, intervalID]);
 
   function startTimer() {
-    console.log('[startTimer] timer started')
+    console.log("[startTimer] timer started");
     const calculated = calculateSeconds(initialTime);
     //if tot seconds != init time in seconds > timer has been already run
     if (timerState === TIMER_STATES["STOPPED"]) {
-      // setIsTiming(true);
-      setTimerState(TIMER_STATES["STARTED"])
-      return
+      setTimerState(TIMER_STATES["STARTED"]);
+      return;
     }
-    // setIsTiming(true);    
-    setTimerState(TIMER_STATES["STARTED"])
+    setTimerState(TIMER_STATES["STARTED"]);
     setTotalSeconds(calculated);
   }
 
   function stopTimer() {
-    // setIsTiming(false);
-    setTimerState(TIMER_STATES["STOPPED"])
-    clearInterval(intervalID)
-    setIntervalID(undefined)
+    setTimerState(TIMER_STATES["STOPPED"]);
+    clearInterval(intervalID);
+    setIntervalID(undefined);
   }
 
   function decrementTotalSeconds() {
@@ -78,13 +75,12 @@ function Main() {
   }
 
   function resetTimer() {
-    console.log("[resetTimer] initial time", initialTime)
-    // setIsTiming(false);
-    setTimerState(TIMER_STATES["STOPPED"])
-    clearInterval(intervalID)
-    setIntervalID(undefined)
-    const initialTimeInSeconds = calculateSeconds(initialTime)
-    setTotalSeconds(initialTimeInSeconds)
+    console.log("[resetTimer] initial time", initialTime);
+    setTimerState(TIMER_STATES["STOPPED"]);
+    clearInterval(intervalID);
+    setIntervalID(undefined);
+    const initialTimeInSeconds = calculateSeconds(initialTime);
+    setTotalSeconds(initialTimeInSeconds);
   }
 
   function handleChange(event) {
@@ -114,13 +110,16 @@ function Main() {
     formatted.push(showMin);
     formatted.push(showSec);
     console.log("formatted time", formatted);
-    return formatted
+    return formatted;
   }
-  const formattedTime = displayTime()
-
+  
+  //add rendering logic: show initial timer, on click: edit timer
+  //disable reset button if timer is not running
+  //if timer is running, clicking timer will enter edit timer state
+  
+  const formattedTime = displayTime();
   return (
     <div>
-
       <input
         type="text"
         id="timer"
@@ -128,13 +127,14 @@ function Main() {
         maxLength="6"
         onChange={handleChange}
       ></input>
-      {totalSeconds && (
-        <div>
-          {formattedTime}
-        </div>
+      {totalSeconds && <div>{formattedTime}</div>}
+      {(timerState === TIMER_STATES["INITIAL"] ||
+        timerState === TIMER_STATES["STOPPED"]) && (
+        <button onClick={startTimer}>Start</button>
       )}
-      {(timerState === TIMER_STATES["INITIAL"] || timerState === TIMER_STATES["STOPPED"]) && <button onClick={startTimer}>Start</button>}
-      {timerState === TIMER_STATES["STARTED"] && <button onClick={stopTimer}>Stop</button>}
+      {timerState === TIMER_STATES["STARTED"] && (
+        <button onClick={stopTimer}>Stop</button>
+      )}
       <button onClick={resetTimer}>Reset</button>
     </div>
   );
