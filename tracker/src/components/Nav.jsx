fb07c498f126ca2498b634697638be86d3dd27ba
no-self-required <louism.coding@gmail.com>
2021-12-06 19:51:25 -0500

    add placeholder logo to navbar

--- tracker/src/components/Nav.jsx
+++ tracker/src/components/Nav.jsx
@@ -4,7 +4,10 @@ import "../styling/nav.scss";
 function Nav() {
   return (
     <div className="navbar">
-      <div id="navbar-logo">ZenTracker</div>
+      <div id="navbar-dot-logo">
+        <span id="navbar-dot"></span>
+        <div id="navbar-logo">ZenTracker</div>
+      </div>
       <nav id="navbar-links">
         <div>Timer</div>
         <div>Login</div>
