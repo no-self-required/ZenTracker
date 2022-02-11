043454cfb5c84daa748db2c0f91daa2084182dd1
no-self-required <louism.coding@gmail.com>
2022-02-11 10:07:10 -0500

    readjust input padding

--- tracker/src/App.js
+++ tracker/src/App.js
@@ -1,12 +1,27 @@
-import './App.css';
-import Nav from './components/Nav';
-import Main from './components/Main';
+import "./App.css";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
 
-import "./styling/app.scss"
+import "./styling/app.scss";
+
+import useLocalStorage from "use-local-storage";
 
 function App() {
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme)
+  }
+  
   return (
-    <div className="App">
+    <div className="App" data-theme={theme}>
+      {/* <button onClick={switchTheme}>
+      </button> */}
       <Nav></Nav>
       <Main></Main>
     </div>
