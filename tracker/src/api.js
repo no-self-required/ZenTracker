c4934d7b30486bf87d538773704af044a2b56a14
no-self-required <louism.coding@gmail.com>
2022-02-12 19:47:35 -0500

    add api example function

---
+++ tracker/src/api.js
@@ -0,0 +1,17 @@
+export async function createUser(userId, password) {
+  const response = await axios.post("/", {
+    userId,
+    password,
+    method: "createUser",
+  });
+
+  return response.data.createdAt;
+}
+
+/**
+ * useEffect(() => {
+ *    createUser(testuser1, hunter2).then(createdAt => {
+ *      setState({createAt})
+ * })
+ * })
+ */
