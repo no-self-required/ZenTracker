5c64852eb9bc928d242986381cbe320927082050
no-self-required <louism.coding@gmail.com>
2023-02-06 23:42:18 -0500

    fix text wrap overflow and alignment for log container

--- tracker/src/components/stats/SingleSession.jsx
+++ tracker/src/components/stats/SingleSession.jsx
@@ -1,43 +1,49 @@
-import React, {} from "react";
+import React from "react";
 // import { UserContext } from "../../App";
-import axios from 'axios';
+import axios from "axios";
 
-import '../../styling/profilestats.scss'
-
+import "../../styling/profilestats.scss";
 
 function SingleSession(props) {
   // const { userData, setUserData } = useContext(UserContext);
 
   const id = props.sessionId;
-  
-  //stays as pending? actually gets deleted 
+
+  //stays as pending? actually gets deleted
   async function deleteSession() {
     // const userid = userData.user.id;
-    const userid = props.currentData.user.id
+    const userid = props.currentData.user.id;
     // console.log("current data userid", props.currentData.user.id)
-    
-    await axios.put(`/api/users/${userid}`, {
-      $pull: { sessions: { id: id} }
-    });
 
+    await axios.put(`/api/users/${userid}`, {
+      $pull: { sessions: { id: id } },
+    });
   }
 
   function deleteRefresh() {
     deleteSession();
-    props.setIsUpdated(true)
+    props.setIsUpdated(true);
   }
   return (
     <div>
-      <div>
-      date: {props.date}
-      <br/>
-      length: {props.length}
-      <br/>
-      {props.log && <div>log: {props.log}</div>}
-      </div> 
-      <button className="delete-button" onClick={deleteRefresh}>Delete</button>
+      <div className="session-info-container">
+        <div className="stat-container">
+          date: <div>{props.date}</div>
+        </div>
+        <div className="stat-container">
+          length: <div>{props.length}</div>
+        </div>
+        {props.log && (
+          <div className="stat-container">
+            log: <div id="log-container">{props.log}</div>
+          </div>
+        )}
+      </div>
+      <button className="delete-button" onClick={deleteRefresh}>
+        Delete
+      </button>
     </div>
-  )
+  );
 }
 
 export default SingleSession;
