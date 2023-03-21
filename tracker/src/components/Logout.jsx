1d71984900884bb017acb259df440b444b49a864
no-self-required <louism.coding@gmail.com>
2023-03-21 00:10:37 -0400

    fix conditional rendering for nav ham drop menu

--- tracker/src/components/Logout.jsx
+++ tracker/src/components/Logout.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 import "../styling/logout.scss";
 //clear jwt, usercontext logged in: set user data: false
-function Logout() {
+function Logout(props) {
   const { userData, setUserData } = useContext(UserContext);
   const navigate = useNavigate();
 
@@ -17,6 +17,8 @@ function Logout() {
     })
     localStorage.setItem("token", '');
     localStorage.setItem("udata", '');
+    props.handleShowNavbar();
+    props.handleHam();
     navigate("/");
   }
   return <div className="logout" onClick={logOut}>Log out</div>;
