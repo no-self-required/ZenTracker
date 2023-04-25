23eb1b17fce7cd2961c1cf5b9512b886ba5c0c01
no-self-required <louism.coding@gmail.com>
2023-04-24 20:24:31 -0400

    change procfile

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const usersRouter = require("./routes/users");
 
 mongoose.connect("mongodb://localhost:27017/zentracker-db");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/users", usersRouter);
 
@@ -50,8 +50,8 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
+app.get('/', (req, res) => { res.send('Hello from Express!')});
 
-app.listen(PORT, () => {
-  console.log(`Listening on Port: ${PORT}`);
-});
+app.listen(process.env.PORT || 3000);
+
 
