327e14b12efa0e4f7fbe7154015dcc2aae5778aa
no-self-required <louism.coding@gmail.com>
2023-04-27 20:45:49 -0400

    update proxy inside package lock for client side

--- tracker/src/components/Login.jsx
+++ tracker/src/components/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
     event.preventDefault();
     try {
       const response = await axios.post(
-        "https://zentracker.herokuapp.com/login/",
+        "/api/login",
         JSON.stringify({ username, password }),
         {
           headers: { "Content-Type": "application/json" },
