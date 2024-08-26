5836a41e98c927e8413f35d938f3d6e157ad0aa9
no-self-required <louism.coding@gmail.com>
2024-08-26 18:36:50 -0400

    disable up and down arrow keys

--- tracker/src/components/Main.jsx
+++ tracker/src/components/Main.jsx
@@ -607,6 +607,10 @@ function Main() {
     const targetInput = event.target;
     setTargetInput(targetInput);
 
+    if (event.keyCode === 38 || event.keyCode === 40) {
+      event.preventDefault();
+    }
+    
     if (event.keyCode >= 48 && event.keyCode <= 57) {
       if (focusedInputId === "timerSecond") {
         if (caretPosition === 1) {
